0029464c59b18e7807cea75a4cf5142cbec6e83e
DESKTOP-T526RFM\lenovo <akshatjaiswal521@gmail.com>
2022-09-19 18:29:53 +0530

    finishing up the features

--- src/components/Renderer/Renderer.tsx
+++ src/components/Renderer/Renderer.tsx
@@ -36,6 +36,7 @@ export function Renderer<CustomPropTypes>({
       }
     });
   }, [chunkedOutRenderString, customComponent]);
+
   return <div className={className}>{elements}</div>;
 }
 export default Renderer;
